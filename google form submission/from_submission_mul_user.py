8d77610810a8aef08147f1a92c27016fa1048141
SajibSaha3 <gta3.cse@diu.edu.bd>
2025-04-22 03:41:22 +0600

    update

--- google form submission/from_submission_mul_user.py
+++ google form submission/from_submission_mul_user.py
@@ -1,10 +1,4 @@
-users = [
-    ("sanjida", "sanjida@gmail.com", "01700000000", "Python Django", "I am interested in the Python Django course. Please contact me for more details."),
-    ("rakib", "rakib@yahoo.com", "01711111111", "Motion Graphics", "Looking forward to learning Motion Graphics. Kindly reach out with more info."),
-    ("nazia", "nazia@hotmail.com", "01722222222", "UX/UI Design", "Interested in the UX/UI Design course. Need more details, please."),
-    ("tamim", "tamim@outlook.com", "01733333333", "Adobe Photoshop", "Want to join the Adobe Photoshop course. Please provide more information."),
-    ("mim", "mim@gmail.com", "01744444444", "Adobe Illustrator", "I'm curious about the Adobe Illustrator course. Can you share more?")
-]
+
 users = [
     ("sanjida", "sanjida@gmail.com", "01700000000", "Python Django", "I am interested in the Python Django course. Please contact me for more details."),
     ("rakib", "rakib@yahoo.com", "01711111111", "Motion Graphics", "Looking forward to learning Motion Graphics. Kindly reach out with more info."),
@@ -23,8 +17,15 @@ driver.get('https://www.creativeitinstitute.com/contact-us') # Replace with the 
 driver.maximize_window()    
 time.sleep(2)  # Wait for the page to load  
 
-
-
+for i , (name, email, phone, course, text) in enumerate(users, start=1):
+    # Fill in the form fields
+    print(f"Filling form for user {i}: ... {name} {email} {phone} {course} {text}")
+    driver.find_element(By.NAME, 'name').send_keys(name)
+    time.sleep(2)  # Wait for the page to load
+    driver.find_element(By.NAME, 'email').send_keys(email)
+    driver.find_element(By.NAME, 'name').clear()
+    time.sleep(2)  # Wait for the page to load
+    driver.find_element(By.NAME, 'email').clear()
 
 
 time.sleep(2)  # Wait for the page to load
